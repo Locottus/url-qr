ca5d189be1153f0dd7352016b06755cd6354d8d8
herlich steven gonzalez zambrano <herlich@gmail.com>
2021-01-16 11:00:20 -0600

    first commit

---
+++ portal/library.js
@@ -0,0 +1,12 @@
+var numMotors = 4;
+
+class Motor {
+    constructor(name, status) {
+      this.name = name;
+      this.status = status;
+    }
+  }[numMotors];
+
+
+var currentMotor = 1;
+console.log('starting')
