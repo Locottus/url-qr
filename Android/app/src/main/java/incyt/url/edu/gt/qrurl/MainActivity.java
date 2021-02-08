f9a412017d6ea091eb0aa9d7f3bef095ebc1ffd7
herlich steven gonzalez zambrano <herlich@gmail.com>
2021-02-08 16:28:12 -0600

    added Android Studio QR project

---
+++ Android/app/src/main/java/incyt/url/edu/gt/qrurl/MainActivity.java
@@ -0,0 +1,14 @@
+package incyt.url.edu.gt.qrurl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
