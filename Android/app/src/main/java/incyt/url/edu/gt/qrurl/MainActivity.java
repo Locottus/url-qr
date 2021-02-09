c6dcfc3cc03f7c412cff145de6c5ad7c95fcfd51
herlich steven gonzalez zambrano <herlich@gmail.com>
2021-02-09 09:20:27 -0600

    version 3 qr driver

--- Android/app/src/main/java/incyt/url/edu/gt/qrurl/MainActivity.java
+++ Android/app/src/main/java/incyt/url/edu/gt/qrurl/MainActivity.java
@@ -1,7 +1,10 @@
 package incyt.url.edu.gt.qrurl;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkAccess();
     }
+
+    private void checkAccess() {
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {
+                android.Manifest.permission.INTERNET,
+                android.Manifest.permission.CAMERA,
+        };
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
 }
